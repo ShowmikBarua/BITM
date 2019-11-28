8c90363ba39d1309b7ac8f6daf01119a0fc4a09e
ss <shwmik619@gmail.com>
2019-11-28 23:46:26 +0600

    Dot Net project PERSONAL INFO

---
+++ PersonalInfoApp/PersonalInfoApp/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace PersonalInfoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.rollTextBox = new System.Windows.Forms.TextBox();
+            this.cgpaTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "roll";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "cgpa";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(115, 30);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // rollTextBox
+            // 
+            this.rollTextBox.Location = new System.Drawing.Point(115, 57);
+            this.rollTextBox.Name = "rollTextBox";
+            this.rollTextBox.Size = new System.Drawing.Size(100, 20);
+            this.rollTextBox.TabIndex = 4;
+            // 
+            // cgpaTextBox
+            // 
+            this.cgpaTextBox.Location = new System.Drawing.Point(115, 86);
+            this.cgpaTextBox.Name = "cgpaTextBox";
+            this.cgpaTextBox.Size = new System.Drawing.Size(100, 20);
+            this.cgpaTextBox.TabIndex = 5;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(115, 139);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(225, 139);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(75, 23);
+            this.showAllButton.TabIndex = 7;
+            this.showAllButton.Text = "show all";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(115, 181);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 273);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.cgpaTextBox);
+            this.Controls.Add(this.rollTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox rollTextBox;
+        private System.Windows.Forms.TextBox cgpaTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ PersonalInfoApp/PersonalInfoApp/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonalInfoApp
+{
+    public partial class Form1 : Form
+    {
+        private string name;
+        private string roll;
+        private string cgpa;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            name = nameTextBox.Text;
+            roll = rollTextBox.Text;
+            cgpa = cgpaTextBox.Text;
+            clearAllText();
+        }
+
+        private void clearAllText()
+        {
+            nameTextBox.Clear();
+            cgpaTextBox.Clear();
+            rollTextBox.Clear();
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            clearAllText();
+            nameTextBox.Text = name;
+            cgpaTextBox.Text = cgpa;
+            rollTextBox.Text = roll;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            clearAllText();
+        }
+
+    }
+}
